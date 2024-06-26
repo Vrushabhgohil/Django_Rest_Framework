7fc737a886cd5ebbd1425b2c8fd9eb41e7443ede
vrushabh26 <vrushabh160.rejoice@gmail.com>
2024-06-26 12:38:24 +0530

    Update some feature

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from myapp.views import EmployeeGetandPost, EmployeePutPatchDelete, Employeeofdepartment, departmentGetandPost, departmentPutPatchDelete
+from myapp.views import *
 
 urlpatterns = [
     path("department/getorpost", departmentGetandPost.as_view()),
@@ -8,4 +8,8 @@ urlpatterns = [
     path("employee/getorpost", EmployeeGetandPost.as_view()),
     path("employee/updateordelete/<int:pk>", EmployeePutPatchDelete.as_view()),
     path("employee/department/<int:pk>", Employeeofdepartment),
+    path("employeewithhtml/", TemplateGetEmployee.as_view(),name='employee'),
+    path("departmentwithhtml/", TemplateGetDepartment.as_view(),name='department'),
+    path("empofdeptwithhtml/<int:pk>", TemplateGetEmpOfDept.as_view(),name='employeeofdepartment'),
+    path("employee/registration", FormEmployee.as_view(),name='employeeregistration'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,14 @@
+from django.shortcuts import get_object_or_404, redirect
 from rest_framework.views import APIView,status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from myapp.models import Department, Employee
+from rest_framework.renderers import TemplateHTMLRenderer
 from myapp.serializers import DepartmentSerializers, EmployeeSerializers
 # Create your views here.
 
+
+""" With Django Default Pagess """
 class departmentGetandPost(APIView):
 
     def get(self,request):
@@ -68,14 +72,13 @@ class departmentPutPatchDelete(APIView):
         return Response({"Message":"Department is not found.."},status=status.HTTP_404_NOT_FOUND)
         
 class EmployeeGetandPost(APIView):
-
     def get(self,request):
         """
-            This Function is use to get all the employee
+            This Function is use to get all the department
         """
         emp = Employee.objects.all()
         serializer = EmployeeSerializers(emp,many=True)
-        return Response({"Employee":serializer.data},status=status.HTTP_200_OK)
+        return Response({'Employee' : serializer.data})
     
     def post(self,request):
         """
@@ -131,6 +134,9 @@ class EmployeePutPatchDelete(APIView):
         
 @api_view(['GET'])
 def Employeeofdepartment(self,pk):
+    """
+        this function is for get employes' record in one department
+    """
     try:
         dept =  Department.objects.get(pk=pk)
         emps = Employee.objects.filter(department = dept)
@@ -144,3 +150,64 @@ def Employeeofdepartment(self,pk):
         
     except Exception:
         return Response({"Message":"There is no Department Registerd in this id"},status=status.HTTP_404_NOT_FOUND)
+
+
+""" With Custom Html Pagess """
+
+class TemplateGetEmployee(APIView):
+    """
+        This class is to get employee data in frontend html page
+    """
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'employee.html'
+    
+    def get(self,request):
+        """
+            This Function is use to get all the employee
+        """
+        emp = Employee.objects.all()
+        return Response({"emp":emp})
+class TemplateGetDepartment(APIView):
+    """
+        This class is to get department data in frontend html page
+    """
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'department.html'
+    
+    def get(self,request):
+        """
+            This Function is use to get all the employee
+        """
+        dept = Department.objects.all()
+        return Response({"dept":dept})
+    
+
+class TemplateGetEmpOfDept(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'empofdept.html'
+
+    def get(self,request,pk):
+        try:
+            dept = Department.objects.get(pk=pk)
+            emps = Employee.objects.filter(department_id = dept.id)
+            if dept and emps:
+                return Response({"dept":dept,"emps":emps})
+            elif dept and not emps:   
+                    return Response({"dept":dept,"msg":f"No Employees!! in {dept.name} department you should hire."})
+        except Exception:
+            return Response({"msg":"There is no Department Registerd in this id"})
+        
+class FormEmployee(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'employeeform.html'
+    
+    def get(self, request):
+        serializer = EmployeeSerializers()
+        return Response({'serializer': serializer,"msg":''})
+    
+    def post(self,request):
+        serializer = EmployeeSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return redirect('employee')
+        return Response({'serializer':serializer,"msg":"Something went wrong"})
